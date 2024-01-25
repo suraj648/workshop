6bc365cd10b43859d17f3015fffd1db01f4cbb58
suraj648 <sooraj.001kumar@gmail.com>
2024-01-25 16:06:06 +0530

    cards

--- article/models.py
+++ article/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=2000)
+    img = models.ImageField(upload_to='article_images/',default='default_img.jpg')
+
+    pub_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
--- article/views.py
+++ article/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request,'article/index.html')
+    data = Article.objects.all()
+    return render(request,'article/index.html',{'data':data})
